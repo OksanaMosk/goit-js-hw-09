e84458f284aa8b79b2cb14cfb007f33354af76c2
OksanaMosk <130758466+OksanaMosk@users.noreply.github.com>
2023-08-29 22:29:53 +0000

    Deploying to gh-pages from @ OksanaMosk/goit-js-hw-09@26279b96de6b2c7094d1b4694705e40b7bdbb640 🚀

---
+++ 01-color-switcher.a07163a2.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=null;t.addEventListener("click",(function(){e=setInterval((function(){}))})),n.addEventListener("click",(function(){clearInterval(e),console.log("Interval with id ".concat(e," has stopped!"))}))}();
+//# sourceMappingURL=01-color-switcher.a07163a2.js.map
