03c36fdec3f9deeba3458c4cf4722a459f4ff384
OksanaMosk <130758466+OksanaMosk@users.noreply.github.com>
2023-08-30 13:21:35 +0000

    Deploying to gh-pages from @ OksanaMosk/goit-js-hw-09@88efb5eab6469b242cf3d03173075a5117bc5337 🚀

--- 01-color-switcher.3c34eda3.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n=null;t.addEventListener("click",(()=>{n=setInterval((()=>{}))})),e.addEventListener("click",(()=>{clearInterval(n),console.log(`Interval with id ${n} has stopped!`)}));
-//# sourceMappingURL=01-color-switcher.3c34eda3.js.map
