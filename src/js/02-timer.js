c669e78d8d74853cdd75bbcbaef3bf51afdcbae9
OksanaMosk <130758466+OksanaMosk@users.noreply.github.com>
2023-08-30 16:57:31 +0300

    2-3

---
+++ src/js/02-timer.js
@@ -0,0 +1,19 @@
+
+
+const flatpickr = require("flatpickr");
+
+import flatpickr from "flatpickr";
+
+import "flatpickr/dist/flatpickr.min.css";
+
+flatpickr(selector, options)
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
