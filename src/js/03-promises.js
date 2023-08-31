c7829164762847bcd8dcee9105d0ada267fd93b7
OksanaMosk <oks-m3@ukr.net>
2023-09-01 02:08:49 +0300

    3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,18 +1,38 @@
 import Notiflix from 'notiflix';
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', handlerSubmit);
+function handlerSubmit(e) {
+  e.preventDefault();
+  let delay = Number(formEl.delay.value);
+  for (let i = 1; i <= formEl.amount.value; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += Number(formEl.step.value);
   }
 }
-
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+function createPromise(position, delay) {
+  const promiseEl = { position, delay };
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(promiseEl);
+      } else {
+        // Reject
+        reject(promiseEl);
+      }
+    }, delay);
   });
+}
